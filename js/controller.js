78d5daa43252d7f69765ccbc1441ca5eee72df08
Juan Salvatore <juansalvatore@live.com.ar>
2017-12-04 13:20:35 -0300

    Create pagination container first version

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,11 @@
 window.onload = function(){
-  
+  // right arrow functionality
+  document.getElementById('rightArrow').onclick = function() {
+    document.getElementById('leftArrow').className = "be-still";
+  }
+
+  // left arrow functionality
+  document.getElementById('leftArrow').onclick = function() {
+    document.getElementById('leftArrow').className = "be-still";
+  }
 }
